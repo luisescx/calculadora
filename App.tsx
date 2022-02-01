23b360c75d28e7d9a9f3d1d9f8a418e4951681e5
luisescx <luis.esculapio@Live.com>
2022-01-31 22:30:29 -0300

    Feat: added calculator screen on App.ts

--- App.tsx
+++ App.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import {Text, View} from 'react-native';
 import {ThemeProvider} from 'styled-components/native';
+import Calculator from './src/screens/Calculator';
 import theme from './src/theme';
 
 const App = () => {
   return (
+    // colocar status bar
     <ThemeProvider theme={theme}>
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Text
-          style={{
-            fontFamily: theme.FONTS.TEXT,
-          }}>
-          App Calculadora
-        </Text>
-      </View>
+      <Calculator />
     </ThemeProvider>
   );
 };
